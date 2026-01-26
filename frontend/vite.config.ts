9bb3bd44afccf5f31da8e3c66c0c4840d5330e65
袁兀 <yuanwu@qianxin.com>
2026-01-26 18:55:13 +0800

    fix:localhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true
       },
       '/health': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true
       }
     }
